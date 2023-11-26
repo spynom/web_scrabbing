94b4c0fae0a635961580c7f731b435faa0c581f2
Saurav Kumar <spynom01@gmail.com>
2023-11-26 20:44:00 +0530

    second commit

--- webscrapping.py
+++ webscrapping.py
@@ -4,7 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+
 driver.get('https://www.google.com')
 time.sleep(2)
 google_search=driver.find_element(by=By.XPATH, value='//*[@id="APjFqb"]')
